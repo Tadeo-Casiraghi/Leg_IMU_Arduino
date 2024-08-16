ca33de21707e20df8827042fcffcebc7684ebd1d
Tadeo <tadeo.casiraghi@gmail.com>
2024-08-16 20:45:55 -0300

    continuing

--- src/classes/imu.py
+++ src/classes/imu.py
@@ -3,28 +3,41 @@ import numpy as np
 class IMU:
 
     def __init__(self):
-        self.settings = ['ax', 'ay', 'az', 'gx', 'gy', 'gz']
-        self.data = {}
+        self.settings = ['a', 'g']
+        self.accel = []
+        self.gyro = []
+        self.calib_accel = []
+        self.calib_gyro = []
         self.times = np.array([])
-        for setting in self.settings:
-            self.data[setting] = np.array([])
+        self.calib_times = np.array([])
         self.gyro_scale = (250/16384) * np.pi/360
         self.accel_scale = 9.81/2048
         
-    def decode_data(self, data):
+    def decode_data(self, data, calibration = False):
         pointer = 0
         for setting in self.settings:
-            if 'a' in setting:
-                scale = self.accel_scale
+            temp = np.array([0,0,0]).T
+            for i in range(3):
+                if data[pointer] >= 128:
+                    value = ((data[pointer]<<8)+ data[pointer+1]) - 2**16
+                else:
+                    value = (data[pointer]<<8)+ data[pointer+1]
+                temp[i] = value
+                pointer += 2
+
+            if calibration and 'a' in setting:
+                self.calib_accel.append(temp * self.accel_scale )
+            elif calibration:
+                self.calib_gyro.append(temp * self.gyro_scale)
+            elif 'a' in setting:
+                self.accel.append(temp * self.accel_scale)
             else:
-                scale = self.gyro_scale
-            if data[pointer] >= 128:
-                value = ((data[pointer]<<8)+ data[pointer+1]) - 2**16
-            else:
-                value = (data[pointer]<<8)+ data[pointer+1]
-            self.data[setting] = np.append(self.data[setting], value * scale )
-            pointer += 2
+                self.gyro.append(temp * self.gyro_scale)
+            
     
-    def decode_time(self, data):
+    def decode_time(self, data, calibration = False):
         value = (data[0]<<24) + (data[0+1]<<16) + (data[0+2]<<8) + (data[0+3])
-        self.times = np.append(self.times, value/1000000)
+        if calibration:
+            self.calib_times = np.append(self.calib_times, value/1000000)
+        else:
+            self.times = np.append(self.times, value/1000000)
--- src/classes/imu_decoder.py
+++ src/classes/imu_decoder.py
@@ -18,9 +18,12 @@ class IMUinterpreter:
             list_of_files = glob.glob(os.path.join(data_directory,'*.bin'))
             list_of_files.sort()
             # TODO: use calibration and other data
-
+            calib_filename = list_of_files[file_number -1]
             filename = list_of_files[file_number]
         
+        with open(calib_filename,'rb') as file:
+            self.calib_data = file.read()
+
         with open(filename,'rb') as file:
             self.raw = file.read()
 
@@ -30,14 +33,45 @@ class IMUinterpreter:
         for _ in range(3):
             self.imus.append(IMU())
 
-        pointer = 0
+        self.decode(self.raw)
+        self.decode(self.calib_data, calibration = True)
 
-        while pointer < len(self.raw)-self.packetsize:
+        self.times = self.imus[0].times
+        self.calib_times = self.imus[0].calib_times
+        
+
+    def decode(self, data, calibration = False):
+        pointer = 0
+        while pointer < len(data)-self.packetsize:
             for imu in self.imus:
-                imu.decode_data(self.raw[pointer:(pointer + self.packetsize - 4)])
+                imu.decode_data(data[pointer:(pointer + self.packetsize - 4)], calibration)
             pointer += self.packetsize - 4
             for imu in self.imus:
-                imu.decode_time(self.raw[pointer:(pointer+4)])
+                imu.decode_time(self.raw[pointer:(pointer+4)], calibration)
             pointer += 4
         
-        self.times = self.imus[0].times
+    def get_gdot(self, g, t):
+        dt = np.average(np.diff(t))
+        gdot = []
+        for g0, g1, g2, g3 in zip(g[:-4], g[1:-3], g[3:-1], g[4:]):
+            gdot.append((g0 - 8*g1 + 8*g2 - g3)/(12*dt))
+        return np.array(gdot)
+
+    def generate_dataset(self, a1, a2, g1, g2, g1dot, g2dot, t):
+        S = [[a1[2], a2[2], g1[2], g2[2], g1dot[0], g2dot[0]]]
+        tprev = t[2]
+        for index, tim in enumerate(t[2:-2]):
+            if tim - tprev > 0.1:
+                S.append([a1[2+index], a2[2+index], g1[2+index], g2[2+index], g1dot[index], g2dot[index]])
+                tprev = tim
+        return S
+    
+    def calibrate_pair(self, a1, a2, g1, g2, g1dot, g2dot, t):
+        S = self.generate_dataset(a1, a2, g1, g2, g1dot, g2dot, t)
+        # TODO: continue here
+    
+    def calibrate(self,):
+        self.gdots = []
+        for imu in self.imus:
+            self.gdots.append(self.get_gdot(imu.calib_gyro, imu.calib_times))
+        
